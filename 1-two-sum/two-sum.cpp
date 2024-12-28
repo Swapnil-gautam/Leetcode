8f4142dc3534013db93ae830649d271d8cfbdf0f
Swapnil Gautam <51311257+Swapnil-gautam@users.noreply.github.com>
2024-12-28 18:41:09 -0500

    Time: 2 ms (81.58%) | Memory: 14.7 MB (30.19%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -3,9 +3,9 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> map;
         vector<int> res;
-        for(int i = 0; i < nums.size(); i++){
-            map[nums[i]] = i;
-        }
+        // for(int i = 0; i < nums.size(); i++){
+        //     map[nums[i]] = i;
+        // }
 
         for(int i = 0; i < nums.size(); i++){
             if(map.find(target - nums[i]) != map.end() && map[(target - nums[i])] != i){
@@ -13,6 +13,7 @@ public:
                 res.push_back(map[(target - nums[i])]);
                 break;
             }
+            map[nums[i]] = i;
         }
 
         return res;
