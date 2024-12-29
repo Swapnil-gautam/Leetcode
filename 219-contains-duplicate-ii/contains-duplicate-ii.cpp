c90ba5fcd160c9350d360a3a6e2bb8a19e0da719
Swapnil Gautam <51311257+Swapnil-gautam@users.noreply.github.com>
2024-12-29 14:41:33 -0500

    Time: 75 ms (26.49%) | Memory: 82 MB (16.78%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -6,36 +6,18 @@ public:
 
         if(k<n){
             for(int i = 0; i<=k; i++){
-                 cout << "k nums[i] entered: " << nums[i] << endl; 
+                //cout << "k nums[i] entered: " << nums[i] << endl; 
                 mapi[nums[i]]++;
-            }
-
-            for(int i = 0; i< n; i++){
-                cout << " i: " << i << " nums[i] removed: " << nums[i] << endl;
-                mapi[nums[i]] = mapi[nums[i]] - 1;
-                if(mapi.find(nums[i])!=mapi.end()){
-                    if(mapi[nums[i]]>0){
-                        return true;
-                    }
-                }
-
-                if(i+k+1 < n){
-                    cout << " nums[i+k] added: " << nums[i+k+1] << endl;
-                    mapi[nums[i+k+1]] = mapi[nums[i+k+1]] + 1;
-                }
             }
         }else{
             for(int i = 0; i<n; i++){
                 // cout << "n nums[i] entered: " << nums[i] << endl; 
                 mapi[nums[i]]++;
             }
+        }
 
-            for(int i = 0; i< n; i++){
-                // if(nums[i] == nums[i+k]){
-                //     return true;
-                // }
-
-                // cout << " i: " << i << " nums[i] removed: " << nums[i] << endl;
+        for(int i = 0; i< n; i++){
+                //cout << " i: " << i << " nums[i] removed: " << nums[i] << endl;
                 mapi[nums[i]] = mapi[nums[i]] - 1;
                 if(mapi.find(nums[i])!=mapi.end()){
                     if(mapi[nums[i]]>0){
@@ -44,11 +26,10 @@ public:
                 }
 
                 if(i+k+1 < n){
-                    // cout << " nums[i+k] added: " << nums[i+k+1] << endl;
+                    //cout << " nums[i+k] added: " << nums[i+k+1] << endl;
                     mapi[nums[i+k+1]] = mapi[nums[i+k+1]] + 1;
                 }
             }
-        }
 
         return false;
     }
