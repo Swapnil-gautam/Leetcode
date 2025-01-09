5546eafaa2acf44221d60cbf4ad60056fa4dd49b
Swapnil Gautam <51311257+Swapnil-gautam@users.noreply.github.com>
2025-01-09 01:06:01 -0500

    Time: 11 ms (48.13%) | Memory: 18.5 MB (87.62%) - LeetSync

--- 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.cpp
+++ 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.cpp
@@ -16,43 +16,95 @@ public:
 };
 */
 
+// class Solution {
+// public:
+//     Node* connect(Node* root) {
+//         if(root == NULL){
+//             return NULL;
+//         }
+//         queue<Node*> level;
+//         level.push(root);
+//         int l = 1;
+//         while(!level.empty()){
+//             int size = level.size();
+//             // cout << "l: " << l << " : ";
+//             while(size>0){
+//                 Node* n = level.front();
+
+//                 level.pop();
+//                 size--;
+//                 // cout << n->val << " ";
+//                 if(n->left != NULL){
+//                     level.push(n->left);
+//                 }
+
+//                 if(n->right != NULL){
+//                     level.push(n->right);
+//                 }
+
+//                 if(size>0){
+//                     n->next = level.front();
+//                 }else{
+//                     n->next = NULL;
+//                 }
+                
+//             }
+//             // cout << endl;
+//             l++;
+//         }
+
+//         return root;
+//     }
+// };
+
 class Solution {
 public:
     Node* connect(Node* root) {
-        if(root == NULL){
-            return NULL;
+        if (root == nullptr) {
+            return nullptr;
         }
-        queue<Node*> level;
-        level.push(root);
-        int l = 1;
-        while(!level.empty()){
-            int size = level.size();
-            // cout << "l: " << l << " : ";
-            while(size>0){
-                Node* n = level.front();
 
-                level.pop();
-                size--;
-                // cout << n->val << " ";
-                if(n->left != NULL){
-                    level.push(n->left);
+        Node* levelStart = root;
+
+        while (levelStart != nullptr) {
+            Node* curr = levelStart;
+            Node* dummy = new Node(0); // Dummy node to represent the start of the next level
+            Node* prev = dummy;
+
+            while (curr != nullptr) {
+                //cout << "curr1: "<< curr->val << endl;
+                //cout << "prev1: "<< prev->val << endl;
+
+                if (curr->left != nullptr) {
+                    prev->next = curr->left;
+                    prev = prev->next;
+                    //cout << "prev2: "<< prev->val << endl;
                 }
 
-                if(n->right != NULL){
-                    level.push(n->right);
-                }
-
-                if(size>0){
-                    n->next = level.front();
-                }else{
-                    n->next = NULL;
+                if (curr->right != nullptr) {
+                    prev->next = curr->right;
+                    prev = prev->next;
+                    //cout << "prev3: "<< prev->val << endl;
                 }
                 
+                // cout << "curr2: "<< curr->val << endl;
+                curr = curr->next;
+                if(curr!=NULL){
+                    // cout << "curr3: "<< curr->val << endl;
+                }
             }
-            // cout << endl;
-            l++;
-        }
 
+            levelStart = dummy->next; // Move to the next level
+            // if(levelStart!=NULL){
+            //     cout << "levelStart: " << levelStart->val << endl;
+            // }
+            delete dummy; // Free the dummy node
+        }
         return root;
     }
 };
+
+
+// Follow-up:
+// You may only use constant extra space.
+// The recursive approach is fine. You may assume implicit stack space does not count as extra space for this problem.
