e519e52b1d579abfb165b643f036db1c2e31ec61
Swapnil Gautam <51311257+Swapnil-gautam@users.noreply.github.com>
2025-01-18 05:59:48 -0500

    Time: 0 ms (100.00%) | Memory: 8.8 MB (51.57%) - LeetSync

--- 433-minimum-genetic-mutation/minimum-genetic-mutation.cpp
+++ 433-minimum-genetic-mutation/minimum-genetic-mutation.cpp
@@ -18,51 +18,21 @@ public:
                     return level;
                 }
 
-                // int j = 0; 
-                // while(j < 8){
-                //     if(curr[j] != endGene[j]){
-                //         string temp;
-                //         temp = curr.substr(0,j) + endGene[j] + curr.substr(j+1,8-(j+1)); 
-                //         cout << "*******" << endl;
-                //         cout <<"0 to j: "<< curr.substr(0,j) << endl;
-                //         cout <<"j: "<< endGene[j] << endl;
-                //         cout <<"j to 8: "<< curr.substr(j+1,8-(j+1)) << endl;
-                //         cout << "*******" << endl;
-                //         cout <<"temp1: " << temp << endl;
-                //         if(mp.find(temp)!=mp.end() && mp[temp] == 0){
-                //             mp[temp] = 1;
-                //             cout <<"temp2: " << temp << endl;
-                //             qu.push(temp);
-                //         }
-                //     }
-                //     j++;
-                // }
                 for(const auto& pair : mp){
-                    // cout << pair.first << endl;
-                    // cout << pair.second << endl;
-                    string tempend = pair.first;
-
-                    int j = 0; 
                     int diff = 0;
-                    while(j < 8){
-                        if(curr[j] != tempend[j]){
+                    for(int j=0; j < 8; j++){
+                        if(curr[j] != pair.first[j]){
                             diff++;
                         }
-                        j++;
                     }
-                    
-                    // cout << "diff: " << diff << endl;
                     if(diff == 1 && pair.second == 0){
                         mp[pair.first] = 1;
                         qu.push(pair.first);
                     }
                 }
-
             }
-            // cout << "%%%%%%%%%%%%%%%%%%" << endl;
-            level = level+1;
+            level++;
         }
-
         return -1;
     }
 };
