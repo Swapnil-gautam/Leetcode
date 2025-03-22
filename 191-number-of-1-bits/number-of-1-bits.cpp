b89604a74f71b11366ec90341387972bea065935
Swapnil Gautam <51311257+Swapnil-gautam@users.noreply.github.com>
2025-03-22 17:50:32 -0400

    Time: 0 ms (100.00%) | Memory: 8.3 MB (47.47%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -3,12 +3,22 @@ public:
     int hammingWeight(int n) {
         // cout << 1%2 << endl;
         int count = 0;
-        while(n > 0){
-            if(n%2 == 1){
+        // while(n > 0){
+        //     if(n%2 == 1){
+        //         count = count + 1;
+        //     }
+        //     n = n/2;
+        // }
+
+        while(n != 0){
+            if(n&1 == 1){ // if last bit is 1
                 count = count + 1;
             }
-            n = n/2;
+            // n = n/2;
+            n = n >> 1;
         }
+
+
         return count;
     }
 };
