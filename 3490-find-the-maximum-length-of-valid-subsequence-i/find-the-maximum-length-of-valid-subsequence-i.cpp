b0f93a61dd4b9f447eb8a70984a2df2b858d529c
Swapnil Gautam <51311257+Swapnil-gautam@users.noreply.github.com>
2025-07-16 19:59:15 -0400

    Time: 7 ms (49.57%) | Memory: 96 MB (83.19%) - LeetSync

--- 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -1,56 +1,70 @@
-// class Solution {
-// public:
-
-//     int solve(vector<int>& nums, int i, int need){
-//         if(i == nums.size()){
-//             return 0;
-//         }
-
-//         int take = 0;
-//         int notake = 0;
-
-//         if(need == (nums[i]%2)){
-
-//         }
-//         take = 1 + solve(nums, i+1 );
-//         notake = solve(nums, i+1 );
-
-//         return max(take, notake);
-
-//     }
-
-//     int maximumLength(vector<int>& nums) {
-//         int i = 0;
-//         int need = 0;
-//         return max(solve(nums, i, need), solve(nums, i, 1));
-//     }
-// };
-
-
-
-
-
 class Solution {
 public:
+
+    // int solve(vector<int>& nums, int i, int need){
+    //     if(i == nums.size()){
+    //         return 0;
+    //     }
+
+    //     int take = 0;
+    //     int notake = 0;
+
+    //     if(need == (nums[i]%2)){
+
+    //     }
+    //     take = 1 + solve(nums, i+1 );
+    //     notake = solve(nums, i+1 );
+
+    //     return max(take, notake);
+
+    // }
+
     int maximumLength(vector<int>& nums) {
-        int countEven = 0, countOdd = 0;
-        for (int num : nums) {
-            if (num % 2 == 0) countEven++;
-            else countOdd++;
-        }
+        // int i = 0;
+        // int need = 0;
+        // return max(solve(nums, i, need), solve(nums, i, 1));
 
-        // Try building alternating parity subsequence
-        int altLen = 1; // At least one number
-        int prevParity = nums[0] % 2;
-
-        for (int i = 1; i < nums.size(); ++i) {
-            int currParity = nums[i] % 2;
-            if (currParity != prevParity) {
-                altLen++;
-                prevParity = currParity;
+        int alleven = 0;
+        int allodd = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i]%2 == 0){
+                alleven++;
+            }else{
+                allodd++;
             }
         }
 
-        return max({countEven, countOdd, altLen});
+
+        int oddeven = 0;
+        int checkfor = 1;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i]%2 == checkfor){
+                oddeven++;
+                if(checkfor == 0){
+                    checkfor = 1;
+                }else{
+                    checkfor = 0;
+                }
+            }
+        }
+
+        int evenodd = 0;
+        checkfor = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i]%2 == checkfor){
+                evenodd++;
+                if(checkfor == 0){
+                    checkfor = 1;
+                }else{
+                    checkfor = 0;
+                }
+            }
+        }
+
+        return max(oddeven, max(evenodd, max(alleven, allodd)));
     }
 };
+
+
+
+
