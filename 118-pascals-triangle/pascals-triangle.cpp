5aaf9d8ceaa2c59bc290317f817091595b516de8
Swapnil Gautam <51311257+Swapnil-gautam@users.noreply.github.com>
2025-08-01 15:35:20 -0400

    Time: 0 ms (100.00%) | Memory: 9.7 MB (62.59%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<int>temp(1,1);
+        // vector<int>temp(1,1);
         vector<vector<int>>res;
 
         // res.push_back(temp);
@@ -9,23 +9,23 @@ public:
 
         for(int i = 0; i < numRows; i++){
             vector<int> newtemp(i+1, 1);
-            // for(int j = 1; j < i+1; j++){
-            //     newtemp[j] = res[i-1][j] + res[i-1][j-1];
-            // }
+            for(int j = 1; j < i; j++){
+                newtemp[j] = res[i-1][j] + res[i-1][j-1];
+            }
             res.push_back(newtemp);
         }
 
-        if(numRows == 1 || numRows == 2 ){
-            return res;
-        }
+        // if(numRows == 1 || numRows == 2 ){
+        //     return res;
+        // }
 
-        for(int i = 2; i < numRows; i++){
-            for(int j = 1; j < i; j++){
-                // cout << "res[i-1][j]: " <<  res[i-1][j] << endl;
-                // cout << "res[i-1][j]: " <<  res[i-1][j] << endl;
-                res[i][j] = res[i-1][j] + res[i-1][j-1];
-            }
-        }
+        // for(int i = 2; i < numRows; i++){
+        //     for(int j = 1; j < i; j++){
+        //         // cout << "res[i-1][j]: " <<  res[i-1][j] << endl;
+        //         // cout << "res[i-1][j]: " <<  res[i-1][j] << endl;
+        //         res[i][j] = res[i-1][j] + res[i-1][j-1];
+        //     }
+        // }
 
         return res;
     }
