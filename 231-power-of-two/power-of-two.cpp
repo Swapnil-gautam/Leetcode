09d4829753505ef1f982ad72c8f44cf0d0b38df9
Swapnil Gautam <51311257+Swapnil-gautam@users.noreply.github.com>
2025-08-09 17:20:42 -0400

    Time: 0 ms (100.00%) | Memory: 7.9 MB (52.80%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,24 +1,48 @@
+// class Solution {
+// public:
+//     bool solve(int n){
+
+//         if(n%2 == 0){
+//             if(n > 2){
+//                 return solve(n/2);
+//             }else{
+//                 return true;
+//             }
+//         }else{
+//             return false;
+//         }
+//     }
+//     bool isPowerOfTwo(int n) {
+//         if(n == 1){
+//             return true;
+//         }
+//         if(n <= 0){
+//             return false;
+//         }
+//         return solve(n);
+//     }
+// };
+
+
 class Solution {
 public:
-    bool solve(int n){
-
-        if(n%2 == 0){
-            if(n > 2){
-                return solve(n/2);
-            }else{
-                return true;
-            }
-        }else{
-            return false;
-        }
-    }
     bool isPowerOfTwo(int n) {
-        if(n == 1){
-            return true;
-        }
         if(n <= 0){
             return false;
         }
-        return solve(n);
+        while(n > 1){
+            // cout << n << endl;
+            if(n & 1 != 0){
+                return false;
+            }
+            n = n >> 1;
+        }
+
+        // cout << n << endl;
+        if(n & 1 != 1){
+            return false;
+        }
+
+        return true;
     }
 };
