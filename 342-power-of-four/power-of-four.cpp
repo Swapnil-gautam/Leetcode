6607018db7821bcf65931a6bd037f067bdd9efa2
Swapnil Gautam <51311257+Swapnil-gautam@users.noreply.github.com>
2025-08-15 16:42:39 -0400

    Time: 0 ms (100.00%) | Memory: 7.7 MB (98.27%) - LeetSync

---
+++ 342-power-of-four/power-of-four.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool solve(int n){
+        if(n <= 0){
+            return false;
+        }
+
+        if(n == 1){
+            return true;
+        }
+
+        if(n % 4 != 0){
+            return false;
+        }else{
+            return solve(n/4);
+        }
+    }
+    bool isPowerOfFour(int n) {
+        return solve(n);
+    }
+};
