6c87876ea6fd67536f424823238eb0d19d99d95c
Swapnil Gautam <51311257+Swapnil-gautam@users.noreply.github.com>
2025-08-18 17:30:44 -0400

    Time: 63 ms (20.85%) | Memory: 30.5 MB (20.40%) - LeetSync

---
+++ 679-24-game/24-game.cpp
@@ -0,0 +1,111 @@
+// class Solution {
+// public:
+//     int sovequ(string equation){
+//         int n = equation.length();
+//         // int i = 0;
+//         string smallequ = "";
+//         // string restofequ = "";
+//         // while(i < n){
+//         //     if(equation[i] != '('){
+//         //         if(equation[i] == ')'){
+//         //             break;
+//         //         }
+//         //         smallequ = smallequ + equation[i];
+//         //     }else{
+//         //         restofequ = restofequ + equation[i];
+//         //     }
+//         //     i++;
+//         // }
+//         // string rest = equation.substr(i, n-i);
+//         // cout << "rest: " << rest << endl;
+//         // cout << "smallequ: " << smallequ << endl;
+//         // cout << "restofequ: " << restofequ << endl;
+//         // cout << "full rest : " << restofequ + " t " + rest << endl;
+
+//         // str t = "0";
+//         // int a = smallequ[]
+
+//         for(int i = 4; i < n; i++ ){
+//             if(equation[i-4] == '(' && equation[i] == ')'){
+//                 smallequ = smallequ + equation.substr(i-4, 5);
+//                 break;
+//             }
+//         }
+//         cout <<"smallequ: " << smallequ << endl;
+
+//         return 0;
+//     }
+
+//     bool judgePoint24(vector<int>& cards) {
+//         string e = "(1+5/2)/3";
+//         cout << "e: " << e << endl;
+//         sovequ(e);
+//         return true;
+//     }
+// };
+
+
+
+
+
+
+
+/************************************************************ C++ ************************************************/
+//Approach (Khandani Backtracking template and all possible options)
+//T.C : O(1), size of input is constant and of size 4
+//S.C : O(1), size of input is constant and of size 4
+class Solution {
+public:
+    double epsilon = 0.1; //0.01, 0.001 etc will work too.
+    // Tolerance for floating-point comparison. To avoid floating point precission errors 
+
+    bool solve(vector<double>& cards) {
+        if(cards.size() == 1) {
+            return abs(cards[0] - 24) <= epsilon;
+        }
+
+        //pick two possible numbers
+        for(int i = 0; i < cards.size(); i++) {
+            for(int j = 0; j < cards.size(); j++) {
+                if(i == j)
+                    continue;
+                
+                vector<double> temp;
+                for(int k = 0; k < cards.size(); k++) {
+                    if(k != i && k != j) {
+                        temp.push_back(cards[k]);
+                    }
+                }
+
+                double a = cards[i];
+                double b = cards[j];
+                vector<double> possibleVal = {a+b, a-b, b-a, a*b}; //b-a is not required, our i and j for loop will take care of it
+
+                if(abs(b) > 0.0) {
+                    possibleVal.push_back(a/b);
+                }
+
+                if(abs(a) > 0.0) {
+                    possibleVal.push_back(b/a); //b/a is not required, our i and j for loop will take care of it
+                }
+
+                for(double val : possibleVal) {
+                    temp.push_back(val); //Do
+                    if(solve(temp) == true) //Explore
+                        return true;
+                    temp.pop_back(); //Undo
+                }
+            }
+        }
+
+        return false;
+    }
+
+    bool judgePoint24(vector<int>& cards) {
+        vector<double> nums;
+        for(int i = 0; i < cards.size(); i++) {
+            nums.push_back(1.0 * cards[i]);
+        }
+        return solve(nums);
+    }
+};
