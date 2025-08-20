3a7151ade970544bbb59787d740159bf9e803bbb
Swapnil Gautam <51311257+Swapnil-gautam@users.noreply.github.com>
2025-08-19 20:22:39 -0400

    Time: 0 ms (100.00%) | Memory: 111.3 MB (40.81%) - LeetSync

--- 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
@@ -1,36 +1,25 @@
-// class Solution {
-// public:
-//     long long zeroFilledSubarray(vector<int>& nums) {
-        
-//     }
-// };
-
 class Solution {
 public:
     long long zeroFilledSubarray(vector<int>& nums) {
-        long long result = 0;
-        
-        int n = nums.size();
-        
         int i = 0;
-        while(i < n) {
-            
-            long long zeros = 0;
-            
-            if(nums[i] == 0) {
-                
-                while(i < n && nums[i] == 0) {
-                    i++;
-                    zeros++;
+        long long zerocount = 0.0;
+        int n = nums.size();
+        long long sum = 0.0;
+        while(i < n){
+            if(nums[i] == 0){
+                zerocount++;
+            }else{
+                if(zerocount != 0){
+                    sum += ((zerocount * (zerocount+1))/2);
                 }
-                
-            } else {
-                    i++;
+                // cout << "zerocount: " << zerocount << " i: " << i << endl;
+                zerocount = 0;
             }
-            result += (zeros)*(zeros+1)/2;
+            i++;
         }
-        
-        return result;
-        
+        if(zerocount != 0){
+            sum += ((zerocount * (zerocount+1))/2);
+        }
+        return sum;
     }
 };
