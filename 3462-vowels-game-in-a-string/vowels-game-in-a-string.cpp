54e9f18b53d8d6b0cd6e5e21d2a3be0b95def5cb
Swapnil Gautam <51311257+Swapnil-gautam@users.noreply.github.com>
2025-09-12 17:37:36 -0400

    Time: 15 ms (18.01%) | Memory: 19 MB (54.04%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -4,40 +4,10 @@ public:
 
 
         int vowelCount = 0;
-        int lastOddVowel = 0;
-        int lastEvenVowel = 0;
-        int chance = 0;
-        // while(s.length() != 0){
-
-        //     for(int i = 0; i < s.length(); i++){
-        //         if(s[i]='a' || s[i]='e'|| s[i]='i'|| s[i]='o'|| s[i]='u'){
-        //             vowelCount++;
-        //             if(vowelCount%2 == 0){
-        //                 lastEvenVowel = i;
-        //             }else{
-        //                 lastOddVowel = i;
-        //             }
-        //         }
-        //     }
-
-        //     if(chance%2 == 0){
-        //         cout << lastOddVowel
-        //         s = s.substr(lastOddVowel, s.length() - lastOddVowel);
-        //     }else{
-        //         s = s.substr(lastEvenVowel, s.length() - lastEvenVowel);
-        //     }
-
-        // }
-
 
         for(int i = 0; i < s.length(); i++){
             if(s[i]=='a' || s[i]=='e'|| s[i]=='i'|| s[i]=='o'|| s[i]=='u'){
                 vowelCount++;
-                // if(vowelCount%2 == 0){
-                //     lastEvenVowel = i;
-                // }else{
-                //     lastOddVowel = i;
-                // }
             }
         }
 
@@ -45,10 +15,9 @@ public:
             return false;
         }
 
-        if(vowelCount%2 == 0){
-            return true;
-        }
-        
+        // if(vowelCount%2 == 0){
+        //     return true;
+        // }
         return true;
 
     }
