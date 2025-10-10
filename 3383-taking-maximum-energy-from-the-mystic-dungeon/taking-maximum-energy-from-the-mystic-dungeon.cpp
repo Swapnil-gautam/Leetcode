93afd5e422d9f656d57c80aa7721847b27d63eea
Swapnil Gautam <51311257+Swapnil-gautam@users.noreply.github.com>
2025-10-10 19:13:00 -0400

    Time: 159 ms (11.55%) | Memory: 145.2 MB (99.67%) - LeetSync

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     int maximumEnergy(vector<int>& energy, int k) {
-        for(int i = energy.size()-1-k; i >= 0; i--){
-            energy[i] = energy[i]+energy[i+k];
-        }
 
         int max_enery = INT_MIN;
-        for(int i = 0; i < energy.size(); i++){
+
+        for(int i = energy.size()-1; i >= 0; i--){
+            if(i <= energy.size()-1-k ){
+                energy[i] = energy[i]+energy[i+k];
+            }
+            
             if(max_enery < energy[i]){
                 max_enery = energy[i];
             }
-        }
+        }        
+        // for(int i = 0; i < energy.size(); i++){
+        //     if(max_enery < energy[i]){
+        //         max_enery = energy[i];
+        //     }
+        // }
         return max_enery;
     }
 };
