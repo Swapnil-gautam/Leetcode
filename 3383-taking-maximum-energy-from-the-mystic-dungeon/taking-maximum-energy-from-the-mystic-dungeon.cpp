bb8090e7040cb3a8ffcbcba1d296674de624fd23
Swapnil Gautam <51311257+Swapnil-gautam@users.noreply.github.com>
2025-10-10 18:59:31 -0400

    Time: 136 ms (72.28%) | Memory: 151.6 MB (57.76%) - LeetSync

---
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -0,0 +1,23 @@
+// class Solution {
+// public:
+//     int maximumEnergy(vector<int>& energy, int k) {
+        
+//     }
+// };
+
+class Solution {
+public:
+    int maximumEnergy(vector<int>& energy, int k) {
+        int n = energy.size();
+        vector<int> t(n, 0); // DP array
+
+        for (int i = n - 1; i >= 0; i--) { // see my video to understand why right to left traversal is needed here
+            if (i + k < n)
+                t[i] = energy[i] + t[i + k];
+            else
+                t[i] = energy[i];
+        }
+
+        return *max_element(begin(t), end(t));
+    }
+};
