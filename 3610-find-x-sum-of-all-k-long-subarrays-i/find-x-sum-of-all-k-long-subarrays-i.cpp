96363c3226373b80f1a8ebe168d40605e094d305
Swapnil Gautam <51311257+Swapnil-gautam@users.noreply.github.com>
2025-11-03 20:31:26 -0500

    Time: 15 ms (56.55%) | Memory: 34.5 MB (52.40%) - LeetSync

--- 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.cpp
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.cpp
@@ -8,27 +8,27 @@ public:
         vector<int> res;
         while(j < nums.size()){
             while(j - i  < k){
-                cout << "nums[j]: " << nums[j] << endl;
+                // cout << "nums[j]: " << nums[j] << endl;
                 k_freq[nums[j]]++;
                 j++;
             }
             priority_queue<pair<int, int>> max_pq; 
             for(auto& kf: k_freq){
-                cout << "added to max pq: " << kf.second << " " <<  kf.first << endl;
+                // cout << "added to max pq: " << kf.second << " " <<  kf.first << endl;
                 max_pq.push({kf.second, kf.first});
             }
             int sum = 0;
             int m = max_pq.size();
             int t = min(x, m);
             for(int i = 0; i < t; i++){
-                cout << max_pq.top().first << " " << max_pq.top().second << endl;
+                // cout << max_pq.top().first << " " << max_pq.top().second << endl;
                 sum = sum + (max_pq.top().first*max_pq.top().second);
                 max_pq.pop();
             }
             res.push_back(sum);
             k_freq[nums[i]]--;
             i++;
-            cout << "i: " << i << endl;
+            // cout << "i: " << i << endl;
         }
         return res;
     }
