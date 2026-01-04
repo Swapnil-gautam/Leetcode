e8e4d4a06341b39008f0b4dbe0d31d3759b68ef4
Swapnil Gautam <51311257+Swapnil-gautam@users.noreply.github.com>
2026-01-04 18:50:31 -0500

    Time: 35 ms (42.68%) | Memory: 16.2 MB (71.46%) - LeetSync

---
+++ 1284-four-divisors/four-divisors.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int sumFourDivisors(vector<int>& nums) {
+        int res = 0;
+        for(int i = 0; i < nums.size(); i++){
+            // cout << floor(sqrt(nums[i])) << endl; 
+            int divisorcount = 0;
+            int sum = 0;
+            for(int j = 1; j <= floor(sqrt(nums[i])); j++){
+                if(nums[i]%j == 0){
+                    // cout << "j: " <<  j << endl;
+                    if(j == nums[i]/j){
+                        divisorcount = divisorcount + 1;
+                        sum = sum + j + nums[i]/j;
+                    }else{
+                        divisorcount = divisorcount + 2;
+                        sum = sum + j + nums[i]/j;
+                    }
+                    
+                }
+                if(divisorcount > 4){
+                    break;
+                }
+            }
+            if(divisorcount == 4){
+                // cout << "sum: " << sum << " nums[i]: " << nums[i] <<  endl;
+                res = res + sum;
+            }
+        }
+        return res;
+    }
+};
