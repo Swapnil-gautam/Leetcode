c2006f4ecbb6eb6838c55da0db41698b1c1e8990
Swapnil Gautam <51311257+Swapnil-gautam@users.noreply.github.com>
2026-05-05 01:15:03 -0400

    Time: 0 ms (100.00%) | Memory: 16.5 MB (31.20%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -11,32 +11,50 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if(head == NULL || head->next == NULL){
-            return head;
+        
+        if( head == NULL){
+            return NULL;
         }
+        int n = 0;
+        ListNode* curr = head;
 
-        int len = 1;
-        ListNode *c = head;
-        while(c->next!=NULL){
-            c = c->next;
-            len++;
+        // cout << curr->val << " " << curr->next->val << endl;
+        while (curr != NULL) {
+            n++;
+            curr = curr->next;
         }
-
-        k = k%len;
+        // cout << "n: " << n << endl;
+        k = k%n;
         if(k == 0){
             return head;
         }
-
-        c->next = head;
-
-        c = head;
-        for(int i = 1; i < (len-k); i++){
-            c = c->next;
+        curr = head;
+        ListNode* newhead;
+        
+        int i = 0;
+        int t = n-k;
+        while (i < t-1) {
+            // cout << "i: " << i << " val: " << curr->val << endl;
+            curr = curr->next;
+            i = i+1;
         }
 
-        ListNode *res = c->next;
-        c->next = NULL;
+        // cout << " val: " << curr->val << endl;
+        newhead = curr->next;
+        curr->next = NULL;
 
-        return res;
+        curr = newhead;
+        // cout << newhead->val << endl;
+
+        while(curr->next != NULL){
+            curr = curr->next;
+        }
+
+        curr->next = head;
+
+
+        return newhead;
+
     }
+    
 };
