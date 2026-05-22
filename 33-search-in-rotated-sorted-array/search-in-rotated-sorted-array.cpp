cb837a15f53eb26141c390ddeb4ad1d399cbb929
Swapnil Gautam <51311257+Swapnil-gautam@users.noreply.github.com>
2026-05-22 16:48:55 -0400

    Time: 0 ms (100.00%) | Memory: 15.2 MB (66.52%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,33 +1,62 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-
-        int low = 0;
-        int high = nums.size()-1;
-
-        int mid;
-        while(low <= high ){
-            mid = (low + high)/2;
-            if(target == nums[mid]){
-                return mid;
-            }
-
-
-            if(nums[low] <= nums[mid]){
-                if(target > nums[mid] || target < nums[low]){
-                    low = mid + 1;
-                }else{
-                    high = mid - 1;
-                }
+    int findsmallest(vector<int>& nums){
+        int l = 0; 
+        int r = nums.size()-1;
+        while(l < r){
+            int mid = (l+r)/2;
+            if(nums[mid] > nums[r]){
+                l = mid+1;
             }else{
-                if(target < nums[mid] || target > nums[high]){
-                    high = mid - 1;
-                    
+                r = mid;
+            }
+        }
+        return l;
+    }
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int s = findsmallest(nums);
+        // cout <<"s: " << s << endl;
+        if(target == nums[s]){
+            return s;
+        }
+        if(target == nums[n-1]){
+            return n-1;
+        }
+        if(target == nums[0]){
+            return 0;
+        }
+
+        if( target > nums[n-1]){
+            int l = 0;
+            int r = s-1;
+            // cout << "this is called 0" << "l: " << l << " r: " << r << endl;
+            while(l <= r){
+                int mid = (l+r)/2;
+                if( nums[mid] == target){
+                    return mid;
+                }else if(nums[mid] > target){
+                    r = mid-1;
                 }else{
-                    low = mid + 1;
+                    l = mid+1;
+                }
+            }
+        }else{
+            int l = s;
+            int r = n-1;
+            // cout << "this is called " << "l: " << l << " r: " << r << endl;
+            while(l <= r){
+                int mid = (l+r)/2;
+                if( nums[mid] == target){
+                    return mid;
+                }else if(nums[mid] > target){
+                    r = mid-1;
+                }else{
+                    l = mid+1;
                 }
             }
         }
+
         return -1;
     }
 };
