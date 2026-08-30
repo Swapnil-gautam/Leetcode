a5ed2e0c80e6a857f96ba66df0c3f777940d8d80
Swapnil Gautam <51311257+Swapnil-gautam@users.noreply.github.com>
2026-08-30 17:10:08 -0400

    Time: 0 ms (100.00%) | Memory: 92.1 MB (22.32%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
@@ -15,42 +15,20 @@ public:
                 pos_min = i;
             }
         }
-        cout << "maxnum: " <<  maxnum << " pos_max: " << pos_max << endl;
-        cout << "minnum: " <<  minnum << " pos_min: " << pos_min << endl;
+
         int Lp = min(pos_max, pos_min)+1;
         int Rp = max(pos_max, pos_min)+1;
 
-        cout << "Lp: " << Lp << endl; 
-        cout << "Rp: " << Rp << endl; 
-
-
         int midpos = nums.size()/2;
-        cout << "midpos: " << midpos << " nums.size(): " << nums.size() << endl;
-        // if(pos_max < midpos && pos_min < midpos){
-        //     return max(pos_max, pos_min);
-        // }
-        // if(pos_max > midpos && pos_min > midpos){
-        //     return max(pos_max, pos_min);
-        // }
+
         if(Lp == Rp){
             int t = nums.size() - Lp;
             return min(Lp, t) + 1;
         }
 
-
         int a = nums.size() - Lp + 1;
         int b = Lp + (nums.size() - Rp) + 1;
 
         return min(min(a, b), Rp);
-
-        // if(Lp < midpos && Rp > midpos){
-        //     return Lp + (nums.size() - Rp) + 1;
-        // }else if(Lp < midpos && Rp <= midpos){
-        //     cout << "return rp" << endl;
-        //     return Rp;
-        // }else if(Lp >= midpos && Rp > midpos){
-        //     return nums.size() - Lp +1;
-        // }
-        // return -1;
     }
 };
