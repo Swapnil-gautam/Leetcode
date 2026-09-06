3404ae98d6f1af85bc30d8d1797e762f289f44f0
Swapnil Gautam <51311257+Swapnil-gautam@users.noreply.github.com>
2026-09-06 16:26:34 -0400

    Time: 14 ms (83.49%) | Memory: 9 MB (91.36%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -0,0 +1,47 @@
+// class Solution {
+// public:
+//     int solve(int i, string& temp, string& s, string& t, int count){
+//         if(temp.size() > t){
+//             return 0; 
+//         }
+
+        
+//     }
+
+
+//     int numDistinct(string s, string t) {
+//         string temp = 0;
+//         solve(0, temp, s, t, count);
+//     }
+// };
+
+
+
+class Solution {
+public:
+    typedef unsigned long long ull;
+    
+    int numDistinct(string s, string t) {
+        int m = s.length();
+        int n = t.length();
+
+        vector<ull> curr(n+1, 0); //Replace dp[i] with curr
+        vector<ull> prev(n+1, 0); //Replace dp[i-1] with prev
+        
+        prev[0] = curr[0] = 1;
+        
+        for(int i = 1; i<m+1; i++) {
+            
+            for(int j = 1; j<n+1; j++) {
+                
+                if(s[i-1] == t[j-1])
+                    curr[j] = prev[j-1] + prev[j];
+                else
+                    curr[j] = prev[j];
+            }
+            prev = curr;
+        }
+
+        return prev[n];
+    }
+};
