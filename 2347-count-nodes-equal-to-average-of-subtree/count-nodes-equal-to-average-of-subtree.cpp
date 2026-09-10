d03e12c9ddbec441e9ebc9705e57a45f3ea931d5
Swapnil Gautam <51311257+Swapnil-gautam@users.noreply.github.com>
2026-09-10 19:33:05 -0400

    Time: 3 ms (91.37%) | Memory: 15.7 MB (64.58%) - LeetSync

---
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
@@ -0,0 +1,65 @@
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     int checkavg(TreeNode* root){
+//         if(root->val == NULL){
+//             return zero;
+//         }
+//         if((root->left->val + root->right->val + root->val)/3 == root->val){
+
+//         }
+//     }
+
+//     int averageOfSubtree(TreeNode* root) {
+//         return checkavg(root);
+//     }
+// };
+
+
+class Solution {
+public:
+    int result;
+    
+    pair<int, int> solve(TreeNode* root) {
+        if(!root)
+            return {0, 0};
+        
+        pair<int, int> l = solve(root->left);
+        pair<int, int> r = solve(root->right);
+        
+        int leftSum   = l.first;
+        int leftCount = l.second;
+        
+        int rightSum   = r.first;
+        int rightCount = r.second;
+        
+        int SUM   = leftSum + rightSum + root->val;
+        int COUNT = leftCount + rightCount + 1;
+        
+        int avg = SUM/COUNT;
+        
+        if(avg == root->val) {
+            result++;
+        }
+        
+        return {SUM, COUNT};
+    }
+    
+    int averageOfSubtree(TreeNode* root) {
+        result = 0;
+        
+        solve(root);
+        
+        return result;
+    }
+};
